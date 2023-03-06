4f4e9b25ad3daef1950df02e2365f993cc0cc85b
inga911 <inga.banaityte221@gmail.com>
2023-03-06 18:39:28 +0100

    Finished string

--- string/index.php
+++ string/index.php
@@ -64,8 +64,14 @@ echo '<br>' . str_replace($del_vowels, '', $str_d);
 
 //8.
 echo '<h3><b>ASTUNTOJI UZDUOTIS.</b></h3>';
-$str_wars = 'Star Wars: Episode ';
-echo $str_wars.str_repeat(' ', rand(0,5)). rand(1,9) . ' - A New Hope';
+$a = 'Star Wars: Episode '.str_repeat(' ', rand(0,5)). rand(1,9) . ' - A New Hope<br>';
+echo $a;
+
+preg_match('/\d/', $a, $result);
+echo '<pre>';
+print_r($result);
+echo 'Epizodo numeris yra: ' . $result;
+echo '</pre>';
 
 
 
@@ -101,3 +107,27 @@ echo "Atsitiktinis 3-ju lotynisku raidziu string'as: " . getRandomStr($raides);
 
 //11.
 echo '<h3><b>VENUOLIKTOJI UZDUOTIS.</b></h3>';
+$string = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale";
+$str_to_array = str_word_count($string, 1, 'ąų');
+echo '<pre>';
+print_r($str_to_array);
+echo '</pre>';
+
+// $to_str = implode(' ', $str_to_array);
+// echo $to_str;
+
+$masyvas10 = 10;
+$rand_index = array_rand($str_to_array, 10);
+$rand_words = array($str_to_array[$rand_index[0]], 
+                    $str_to_array[$rand_index[1]], 
+                    $str_to_array[$rand_index[2]], 
+                    $str_to_array[$rand_index[3]], 
+                    $str_to_array[$rand_index[4]], 
+                    $str_to_array[$rand_index[5]], 
+                    $str_to_array[$rand_index[6]], 
+                    $str_to_array[$rand_index[7]], 
+                    $str_to_array[$rand_index[8]], 
+                    $str_to_array[$rand_index[9]]);
+$string_from_a = implode(' ', $rand_words);
+echo '<br>' . "<b>String'as iš 10 atsitiktinių žodžių: </b><br>" . $string_from_a;
+
